856bd09f35b8af2ef9e393d9ae3df5cd3e60db79
SimonJagerESS <simon.jager@ess.eu>
2025-04-15 09:48:58 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'r2d2'
+copyright = '2025, ESS ICS Automation'
+author = 'Simon Jäger'
 
 release = '0.1'
 version = '0.1.0'
